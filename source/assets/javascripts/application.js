b44051da49daa44bb005fafc4fa0180ee0350218
Dave Woodall <dave.woodall@livingsocial.com>
2015-03-08 10:48:15 -0600

    Start a TOC with lodash

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,2 @@
 //= require 'main'
+//= require 'lodash.min.js'
--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){
   var $divs = $('.l-container > div');
   $tags = $('#tags')
-  var $sortedDivs = $divs.sort(SortByName);
 
-  $.each($sortedDivs, function(i, div) {
+  var uniqTags = _.uniq($divs, function(div){
+    return $(div).attr('class');
+  });
+
+  _.forEach(uniqTags, function(div) {
     var myDiv = $(div).attr('class');
     var myTags = []
-    myTags.push(myDiv + ", ");
+    myTags.push("<li><a class='toc' data-toc='" + myDiv + "' href='" + myDiv + "'>" + myDiv + "</a></li>");
     $tags.append(myTags);
-  })
+  });
+
+  $('.toc').click(function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    console.log($(this).data());
+  });
+
+  var $sortedDivs = $divs.sort(SortByName);
 
   var $book = $.each($sortedDivs, function(index, div){
     var myClass = $(div).attr('class');
     $(div).prepend("<p class='type'>" + myClass + "</p>")
   })
   $('.l-container').html($book);
+
+
 
   function SortByName(a, b){
     var aName = a.className.toLowerCase();
